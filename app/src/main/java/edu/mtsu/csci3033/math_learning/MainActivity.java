8a4a306d7903deac821c6df28ca2b575bb9f807e
Matthew Vanlandingham <mjvanlandingham@frontier.com>
2017-11-07 01:25:19 -0600

    Added a dropdown menu. Can change this to a menu bar across the top.

--- app/src/main/java/edu/mtsu/csci3033/math_learning/MainActivity.java
+++ app/src/main/java/edu/mtsu/csci3033/math_learning/MainActivity.java
@@ -2,12 +2,58 @@ package edu.mtsu.csci3033.math_learning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private static final String[] paths = {"Home", "Settings", "Difficulty"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_layout);
+
+        spinner = (Spinner) findViewById(R.id.menu_spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                switch(i) {
+
+                    case 0:
+                        break;
+
+                    case 1:
+                        break;
+
+
+                    case 2:
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
+
+
+
+
+
 }
